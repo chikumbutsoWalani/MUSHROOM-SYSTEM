534a24e976b050bf66891ad38f2acbd6d2f1f2bc
root <root@CHIKU.CHIKU>
2020-04-08 14:40:10 +0200

    fixing css 3

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
 
 var app = express();
-
-var date = new Date().getTime();
-date += (2 * 60 * 60 * 1000);
+// 
+// var date = new Date().getTime();
+// date += (2 * 60 * 60 * 1000);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -98,8 +98,7 @@ app.post("/mushroomData", function(req, res) {
     moist3: req.body.moistureThree,
     temp1: req.body.tempSensor1,
     temp2: req.body.tempSensor2,
-    temp3: req.body.tempSensor3,
-    date: new Date().toUTCString()
+    temp3: req.body.tempSensor3
 
   })
   sensor.save()
