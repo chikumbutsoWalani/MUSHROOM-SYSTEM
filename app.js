30c6019c2c05fc9fee52a44a723fa60d70237c85
root <root@CHIKU.CHIKU>
2020-04-09 09:51:18 +0200

    v7

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
 
 var app = express();
-
-var date = new Date().getTime();
-date += (2 * 60 * 60 * 1000);
+// 
+// var date = new Date().getTime();
+// date += (2 * 60 * 60 * 1000);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -34,9 +34,7 @@ const sensorsSchema = mongoose.Schema({
   moist3: Number,
   temp1: Number,
   temp2: Number,
-  temp3: Number,
-  date: String,
-
+  temp3: Number
 });
 
 
@@ -59,7 +57,7 @@ app.get("/values",function(req,res) {
         console.log(result.temp1);
           console.log(result.humid);
       res.render("values",{
-        temp: result.temp1,
+        temp: result.temp,
         humid: result.humid,
         moist1: result.moist1,
         moist2: result.moist2,
@@ -100,8 +98,7 @@ app.post("/mushroomData", function(req, res) {
     moist3: req.body.moistureThree,
     temp1: req.body.tempSensor1,
     temp2: req.body.tempSensor2,
-    temp3: req.body.tempSensor3,
-    date: new Date().toUTCString()
+    temp3: req.body.tempSensor3
 
   })
   sensor.save()
